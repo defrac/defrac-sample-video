e4fe6f2b4d7161003bcf1fe37b39fa53c262b900
Joa Ebert <joa.ebert@defrac.com>
2015-06-15 14:26:59 +0200

    Update for 0.3.9

--- src/java.web/defrac/sample/video/VideoSample.java
+++ src/java.web/defrac/sample/video/VideoSample.java
@@ -1,25 +1,30 @@
 package defrac.sample.video;
 
-import defrac.app.Bootstrap;
-import defrac.app.GenericApp;
 import defrac.display.*;
-import defrac.event.EnterFrameEvent;
+import defrac.display.event.raw.EnterFrameEvent;
 import defrac.event.EventBinding;
-import defrac.event.EventListener;
-import defrac.event.Events;
 import defrac.geom.Point;
 import defrac.gl.GL;
+import defrac.gl.GLSubstrate;
 import defrac.gl.GLTexture;
 import defrac.gl.WebGLSubstrate;
-import defrac.web.*;
+import defrac.ui.DisplayList;
+import defrac.ui.FrameBuilder;
+import defrac.web.HTMLVideoElement;
+import defrac.web.Toplevel;
+import defrac.web.WebGLRenderingContext;
+import defrac.web.Window;
 
 import javax.annotation.Nonnull;
-import java.lang.Math;
 
 /**
  *
  */
-class VideoSample extends GenericApp {
+class VideoSample extends defrac.ui.Screen {
+  public static void main(String[] args) {
+    FrameBuilder.forScreen(new VideoSample()).show();
+  }
+
   static final Window window = Toplevel.window();
 
   static final int VIDEO_WIDTH = 640;
@@ -29,10 +34,7 @@ class VideoSample extends GenericApp {
   static final int VIDEOS_V = 32;
   static final int VIDEOS_TOTAL = VIDEOS_V * VIDEOS_H;
 
-  public static void main(final String[] args) {
-    Bootstrap.run(new VideoSample());
-  }
-
+  DisplayList displayList;
   TextureData textureData;
   HTMLVideoElement video;
   Image[] videos = new Image[VIDEOS_TOTAL];
@@ -42,51 +44,42 @@ class VideoSample extends GenericApp {
 
   @Override
   protected void onCreate() {
-    stage().backgroundColor(0xff000000);
+    super.onCreate();
 
-    // Create the video element
-    video = (HTMLVideoElement)window.document.createElement("video");
+    displayList = new DisplayList();
 
-    // Make sure we can play WebM files
-    if(video.canPlayType("video/webm").length() == 0) {
-      window.alert("Can't play WebM videos :/");
-    }
+    displayList.onStageReady(stage -> {
+      stage.backgroundColor(0xff000000);
 
-    // Set everything up and start playing the video
-    video.autoplay = true;
-    video.src = "com.defrac.sample.video/big-buck-bunny_trailer.webm";
-    video.onplaying = new defrac.web.EventListener<Event>() {
-      @Override
-      public void onEvent(Event event) {
-        onVideoPlaying();
+      // Create the video element
+      video = (HTMLVideoElement)window.document.createElement("video");
+
+      // Make sure we can play WebM files
+      if(video.canPlayType("video/webm").length() == 0) {
+        window.alert("Can't play WebM videos :/");
       }
-    };
-    video.onended = new defrac.web.EventListener<Event>() {
-      @Override
-      public void onEvent(Event event) {
-        Events.onEnterFrame.remove(frameLoop);
-        frameLoop = Events.onEnterFrame.add(new EventListener<EnterFrameEvent>() {
-          @Override
-          public void onEvent(EnterFrameEvent enterFrameEvent) {
-            if (stage().alpha() <= 0.0f) {
-              stage().alpha(0.0f);
-              Events.onEnterFrame.remove(frameLoop);
-            } else {
-              stage().alpha(stage().alpha() - 0.01f);
-            }
+
+      // Set everything up and start playing the video
+      video.autoplay = true;
+      video.src = "com.defrac.sample.video/big-buck-bunny_trailer.webm";
+      video.onplaying = event -> onVideoPlaying(stage);
+      video.onended = event -> {
+        stage.globalEvents().onEnterFrame.remove(frameLoop);
+        frameLoop = stage.globalEvents().onEnterFrame.add(e -> {
+          if (stage.alpha() <= 0.0f) {
+            stage.alpha(0.0f);
+            stage.globalEvents().onEnterFrame.remove(frameLoop);
+          } else {
+            stage.alpha(stage.alpha() - 0.01f);
           }
         });
-      }
-    };
+      };
+    });
+
+    rootView(displayList);
   }
 
-  @Override
-  protected void onCreationFailure(@Nonnull Throwable reason) {
-    window.alert("It looks like your browser doesn't support WebGL :/");
-    super.onCreationFailure(reason);
-  }
-
-  private void onVideoPlaying() {
+  private void onVideoPlaying(@Nonnull final Stage stage) {
     // Since we want to access methods of WebGLRenderingContext we
     // need to go deep down into the OpenGL dungeon and extract
     // the lowest layer, which is the WebGLRenderingContext
@@ -97,25 +90,15 @@ class VideoSample extends GenericApp {
     // We choose transient because it resides only in VRAM and is therefore
     // perfect for this use case
     textureData = TextureData.Transient.fromFactory(
-        new TextureData.Transient.Factory() {
-          @Override
-          public void texImage2D(@Nonnull final TextureData.Transient textureData,
-                                 @Nonnull final GL gl, final int target,
-                                 final int level, final int internalFormat, final int border) {
-            // The TextureData.Transient.fromFactory method allows us to provide
-            // a TextureData that is created on-demand. This call should happen only
-            // once but if a context loss occurs we might get asked again.
-            //
-            // For the web, we want to provide the content of a video as the initial
-            // texture. This is why we need access to the WebGLRenderingContext. It
-            // allows us to upload the HTMLVideoElement as a texture.
-            toWebGL(gl).texImage2D(GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, video);
-            try {
-              gl.assertNoError();
-            } catch (Throwable t) {
-              window.alert(t.getMessage());
-            }
-          }
+        (data, gl, target, level, internalFormat, border) -> {
+          // The TextureData.Transient.fromFactory method allows us to provide
+          // a TextureData that is created on-demand. This call should happen only
+          // once but if a context loss occurs we might get asked again.
+          //
+          // For the web, we want to provide the content of a video as the initial
+          // texture. This is why we need access to the WebGLRenderingContext. It
+          // allows us to upload the HTMLVideoElement as a texture.
+          toWebGL(gl).texImage2D(GL.TEXTURE_2D, 0, GL.RGBA, GL.RGBA, GL.UNSIGNED_BYTE, video);
         },
         VIDEO_WIDTH, VIDEO_HEIGHT,
         TextureDataFormat.RGBA,
@@ -137,33 +120,25 @@ class VideoSample extends GenericApp {
             moveTo(i * videoWidth, j * videoHeight);
         velocities[index] = new Point(random(), random());
         videos[index++] = image;
-        addChild(image);
+        stage.addChild(image);
       }
     }
 
-    frameLoop = Events.onEnterFrame.add(new EventListener<EnterFrameEvent>() {
-      @Override
-      public void onEvent(EnterFrameEvent enterFrameEvent) {
-        onEnterFrame();
-      }
-    });
+    frameLoop = stage.globalEvents().onEnterFrame.add(event -> onEnterFrame(stage));
   }
 
-  void onEnterFrame() {
+  void onEnterFrame(@Nonnull final Stage stage) {
     // We want to update the content of the TextureData on each frame.
     // In order to do so we must get a hold of the GLTexture we want to
     // modify first. This is done by asking a TextureScope for the GLTexture
     // associated with a TextureData. Lucky for us, the Stage is a TextureScope
-    final GLTexture texture = stage().getOrCreateTexture(textureData);
+    final GLTexture texture = stage.getOrCreateTexture(textureData);
 
     // Update the texture
-    stage().execGL(new GLBlock() {
-      @Override
-      public void execGL(@Nonnull GL gl) {
-        gl.bindTexture(GL.TEXTURE_2D, texture);
-        toWebGL(gl).texSubImage2D(GL.TEXTURE_2D, 0, 0, 0, GL.RGBA, GL.UNSIGNED_BYTE, video);
-        gl.bindTexture(GL.TEXTURE_2D, null);
-      }
+    stage.execGL(gl -> {
+      gl.bindTexture(GL.TEXTURE_2D, texture);
+      toWebGL(gl).texSubImage2D(GL.TEXTURE_2D, 0, 0, 0, GL.RGBA, GL.UNSIGNED_BYTE, video);
+      gl.bindTexture(GL.TEXTURE_2D, null);
     });
 
     // Move the videos around ...
@@ -192,24 +167,22 @@ class VideoSample extends GenericApp {
     }
   }
 
-  WebGLRenderingContext toWebGL(GL gl) {
-    // Level #1: The platform agnostic and enriched OpenGL interface
-    //           is the given parameter
-
-    // Level #2: The substrate is the platform specific
-    //           implementation and basis of the enriched API
+  WebGLRenderingContext toWebGL(GLSubstrate gl) {
+    // Level #1: The substrate is the platform specific
+    //           implementation and basis of the enriched defrac.gl.GL API
     //
     //           Note: We know it is a WebGLSubstrate in the case
     //           of the web export and we have to introduce a cast.
-    WebGLSubstrate substrate = (WebGLSubstrate)gl.substrate();
+    //
+    //           We flatten() the substrate because in a  more complex
+    //           application someone might wrap it or enable DisplayList
+    //           debugging which also wraps the substrate.
+    WebGLSubstrate substrate = (WebGLSubstrate)gl.flatten();
 
-    // Level #3: The actual WebGLRenderingContext which the
+    // Level #2: The actual WebGLRenderingContext which the
     //           WebGLSubstrate keeps a reference to. Now we are
     //           looking at the actual WebGL API and can call methods
     //           like texImage2D that take a HTMLVideoElement
-    //
-    //           The GL object we retrieved in level #1 will make calls
-    //           on this object.
     return substrate.context();
   }
 
